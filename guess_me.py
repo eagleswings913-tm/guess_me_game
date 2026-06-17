b58039c1c0df6861e47cb45adfae1a42c588ab87
Tricia Miller <eagleswings913@gmail.com>
2026-06-16 20:08:34 -0400

    Removed debugging lines

--- guess_me.py
+++ guess_me.py
@@ -1,22 +1,10 @@
 
-import sys
 from random import randint
-print('I am in')
-
-#print(sys.argv[0])
-#print(sys.argv[1])
-#print(sys.argv[2])
-
-
-#lower = sys.argv[1]
-#higher = sys.argv[2]
 
 lower = 1
 higher = 10
 
 rand_num = randint(int(lower),int(higher))
-
-#print(rand_num)
 
 while True:
     guess = int(input(f'Enter a number between {lower} and {higher}: '))
@@ -28,6 +16,3 @@ while True:
             print('Try again')
             
 print('You are a genius!')
-
-
-# cd /storage/3037-3961/Python
